b203f0b3954fe9d29f0b98d7efd89770fcba2686
abdulfarukgazi@gmail.com <abdulfarukgazi@gmail.com>
2024-04-21 12:29:50 +0530

    conflict resolved

--- git_demo/file_1.c
+++ git_demo/file_1.c
@@ -1,1 +1,18 @@
-vim file_1.c
+#include <stdio.h>
+int main(){
+
+	int number1,number2,sum;
+
+	printf("Enter two integers:");
+	Scanf("%d %d",&number1,&number2);
+
+	// calculate the sum
+	 sum = number1 + number2;
+
+	 printf("%d + %d = %d",number1,number2,sum);
+	 return 0;
+}
+//function added in branch1
+int func_add(int a,int b)
+
+	return a+b;
